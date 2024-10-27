128f18a8921104d03d7020846b62f442be235b0d
DmitriyBabij <132613323+DmitriyBabij@users.noreply.github.com>
2024-10-27 11:31:09 +0200

    task-2

---
+++ js/task-2.js
@@ -0,0 +1,17 @@
+class storage {
+    #items = [];
+}
+getItems()
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
