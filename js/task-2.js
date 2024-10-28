9865c9b1c7f1c7195b09569f2cb0ab22a501ff13
DmitriyBabij <132613323+DmitriyBabij@users.noreply.github.com>
2024-10-28 17:41:48 +0200

    hw-2 finish

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,26 @@
 class storage {
-    #items = [];
-}
-getItems()
+    #items;
 
+    constructor(items) {
+        this.#items = items;
+}
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        const item = this.#items.indexOf(itemToRemove);
+        if (item !== -1) {
+            this.#items.splice(item, 1);
+        }
+    }
+
+}
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
